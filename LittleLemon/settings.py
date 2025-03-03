3a53d03321759828d7c1a9a7af24fa3911f58bc8
Singh <rsingh@AML6GPPFP9R-11659.local>
2025-03-03 07:03:06 -0600

    set up a home page

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "restaurant"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = "LittleLemon.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['restaurant/templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -113,9 +114,12 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+STATICFILES_DIRS = [
+    'restaurant/static',
+]
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "restaurant/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
